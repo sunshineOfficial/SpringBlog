f91423f2cbfc92b31584741bdb7d1fbd6d192728
Dmitry Khrunin <thesunshineofficial@gmail.com>
2023-09-23 16:59:18 +0300

    Update repositories

--- src/main/java/ru/pnzgu/springblog/repositories/PostRepository.java
+++ src/main/java/ru/pnzgu/springblog/repositories/PostRepository.java
@@ -1,12 +1,13 @@
 package ru.pnzgu.springblog.repositories;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.pnzgu.springblog.models.Post;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
-    List<Post> findAllByUser_Id(int userId);
-    List<Post> findAllByPublished(boolean published);
-    List<Post> findAllByUser_IdAndPublished(int userId, boolean published);
+    List<Post> findAllByUser_Id(int userId, Pageable pageable);
+    List<Post> findAllByPublished(boolean published, Pageable pageable);
+    List<Post> findAllByUser_IdAndPublished(int userId, boolean published, Pageable pageable);
 }
--- src/main/java/ru/pnzgu/springblog/repositories/UserRepository.java
+++ src/main/java/ru/pnzgu/springblog/repositories/UserRepository.java
@@ -1,5 +1,6 @@
 package ru.pnzgu.springblog.repositories;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.pnzgu.springblog.models.UserEntity;
 
@@ -9,6 +10,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     Optional<UserEntity> findByUsername(String username);
     boolean existsByUsername(String username);
-    List<UserEntity> findAllByRole_Id(Integer roleId);
-    List<UserEntity> findAllByRole_Name(String name);
+    List<UserEntity> findAllByRole_Id(Integer roleId, Pageable pageable);
+    List<UserEntity> findAllByRole_Name(String name, Pageable pageable);
 }
